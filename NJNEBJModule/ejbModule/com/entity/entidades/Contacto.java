b2b67c40bd83be4388db874615490ecec97a078c
Zulath <juanirp88@hotmail.com>
2013-06-20 06:37:29 -0300

    Estan las 3 entidades que me parecieron, no se si habra que cambiar algo Ademas hay que ver si les falta algo

---
+++ NJNEBJModule/ejbModule/com/entity/entidades/Contacto.java
@@ -0,0 +1,72 @@
+package com.entity.entidades;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+
+@Entity
+public class Contacto implements Serializable{
+	
+private static final long serialVersionUID = 1L;
+	
+	@Id	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id = 0;
+	private String nombre;
+	private String apellido;
+	
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinColumn(name = "contactoId")
+	private List<Deuda> deudas;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	/**
+	 * Recupero todas las deudas del contacto.
+	 * 
+	 * @return
+	 */
+	public List<Deuda> getDeudas() {
+		return deudas;
+	}
+	
+	/**
+	 * Agrego una deuda al contacto.
+	 * 
+	 * @param deuda
+	 * 
+	 */
+	public void addDeuda(Deuda deuda) {
+		if (deudas == null) {
+			deudas = new ArrayList<Deuda>();
+		}
+		deudas.add(deuda);
+	}
+}
--- NJNEBJModule/ejbModule/com/entity/entidades/Deuda.java
+++ NJNEBJModule/ejbModule/com/entity/entidades/Deuda.java
@@ -1,17 +1,82 @@
 package com.entity.entidades;
 
 import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+/**
+ * Son las deudas (o pagos) de un contacto con el usuario
+ *
+ */
 @Entity
 public class Deuda implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id	
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Integer id = 0;
+	private String descripcion;
+	private String estado;
+	private Date fechaEmision;
+	private Date fechaPago;
+	private int monto;
+	
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinColumn(name = "deudaId")
+	private TipoDeuda tipoDeuda;
+	
+		
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public Date getFechaEmision() {
+		return fechaEmision;
+	}
+	public void setFechaEmision(Date fechaEmision) {
+		this.fechaEmision = fechaEmision;
+	}
+	public Date getFechaPago() {
+		return fechaPago;
+	}
+	public void setFechaPago(Date fechaPago) {
+		this.fechaPago = fechaPago;
+	}
+	public int getMonto() {
+		return monto;
+	}
+	public void setMonto(int monto) {
+		this.monto = monto;
+	}
+	public TipoDeuda getTipoDeuda() {
+		return tipoDeuda;
+	}
+	public void setTipoDeuda(TipoDeuda tipoDeuda) {
+		this.tipoDeuda = tipoDeuda;
+	}
+	
+	
+		
 }
