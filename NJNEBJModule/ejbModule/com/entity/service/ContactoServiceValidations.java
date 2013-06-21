ebcaff3ae4ad17355d28b65d445e76229434c70d
Nicolas Parola <Nicolas Parola@192.168.1.34>
2013-06-21 17:42:41 -0300

    COntactoServiceValidations DeudaServiceValidations

--- NJNEBJModule/ejbModule/com/entity/service/ContactoServiceValidations.java
+++ NJNEBJModule/ejbModule/com/entity/service/ContactoServiceValidations.java
@@ -1,16 +1,39 @@
 package com.entity.service;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
 
 import com.application.exceptions.ValidationError;
 import com.entity.dto.ContactoDTO;
 import com.entity.entidades.Contacto;
+import com.entity.repository.ContactoRepository;
 
+@Stateless
+@LocalBean
 public class ContactoServiceValidations {
+	@EJB
+	ContactoRepository contactoRepository;
 
 	public List<ValidationError> validarAddContacto(ContactoDTO contacto) {
-		// TODO Auto-generated method stub
-		return null;
+		List<ValidationError> errors = new ArrayList<ValidationError>();
+		
+		if(contacto.getId() != null ){
+			Contacto contact = contactoRepository.get(contacto.getId());
+			if (contact != null){
+				   errors.add(new ValidationError("id", "El contacto ya se encuentra creado."));
+			}
+		}
+		
+		if(contacto.getNombre() == null || contacto.getApellido() == null ){
+			errors.add(new ValidationError("nombre-apellido","El nombre y el apellido no pueden estar vacios"));
+		}
+		
+		
+		return errors;
 	}
 
 	public void validarEntityBean(Contacto nuevoContacto) {
